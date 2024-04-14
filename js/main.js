cd65a8625e559f7517b85a01842b171cb3e83ad4
CalenFeaElen <calenfeaelen@gmail.com>
2024-04-14 14:49:18 -0700

    Added mobile styles

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const animatedLogo = document.querySelector(".logo");
+
+//Play the Chelsea Codes the Day logo animation on mouseover
+animatedLogo.addEventListener("mouseover", function () {
+  animatedLogo.play();
+});
